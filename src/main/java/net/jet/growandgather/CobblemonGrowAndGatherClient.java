18dd67293df1ca6dd61541b489bc5d56239d1cd5
Bumble Beescuit <mareep.scarlet@gmail.com>
2026-04-17 21:49:57 +0100

    first commit

---
+++ src/main/java/net/jet/growandgather/CobblemonGrowAndGatherClient.java
@@ -0,0 +1,10 @@
+package net.jet.growandgather;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CobblemonGrowAndGatherClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/jet/growandgather/CobblemonGrowAndGatherDataGenerator.java
@@ -0,0 +1,11 @@
+package net.jet.growandgather;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CobblemonGrowAndGatherDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
